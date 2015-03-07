2158efed9665290f224104742aea6f9e7df63be3
Koby Aharon <kobyahron@gmail.com>
2015-03-07 22:29:36 +0200

    Move ruby installation from provision step to be executed using ssh

--- install-knife-solo.sh
+++ install-knife-solo.sh
@@ -1,4 +1,44 @@
 #!/bin/sh
 
+# Making sure rbenv is installed successfully
+
+which rbenv >> /dev/null 2>&1
+
+if [ $? -gt 0 ]; then
+  echo "rbenv is not installed. Did you provision the machine properly?"
+  exit 1
+fi
+
+rbenv install 2.1.5
+rbenv global 2.1.5
+ruby -v
+
+if [ $? -gt 0 ]; then
+  echo "Error installing ruby. See log above"
+  exit 1
+fi
+
+# Configure rubygems to ignore documentation and install bundler
+echo "gem: --no-ri --no-rdoc" > /home/vagrant/.gemrc
+
+# Installing Ruby --- End ---
+
+echo "Installing chef solo gems"
+
+export USE_SYSTEM_GECODE=1
+
 gem install -V 'knife-solo'
+
+if [ $? -gt 0 ]; then
+  echo "Error installing knife-solo. See log above"
+  exit 1
+fi
+
 gem install -V 'berkshelf'
+
+if [ $? -gt 0 ]; then
+  echo "Error installing berkshelf. See log above"
+  exit 1
+fi
+
+echo "Finish installing chef solo gems"
--- provision.sh
+++ provision.sh
@@ -7,52 +7,30 @@ sudo yum install -y git-core curl zlib1g-dev build-essential libssl-dev libreadl
 
 # Install ruby using rbenv
 
-#RBENV_FOLDER=/home/vagrant/.rbenv
-#RBENV_FILE=/home/vagrant/.bashrc
 RBENV_FOLDER=/usr/local/rbenv
 RBENV_FILE=/etc/profile.d/rbenv.sh
 
-echo 'export RBENV_FOLDER="$( echo $RBENV_FOLDER )"' > $RBENV_FILE
+groupadd rubies
+usermod -a -G rubies vagrant
 
-#su vagrant
-#cd /home/vagrant
+echo "export RBENV_ROOT=$RBENV_FOLDER" > $RBENV_FILE
+
 git clone git://github.com/sstephenson/rbenv.git $RBENV_FOLDER
-#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
-echo 'export PATH="$RBENV_FOLDER/bin:$PATH"' >> $RBENV_FILE
+echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> $RBENV_FILE
 
 git clone git://github.com/sstephenson/ruby-build.git $RBENV_FOLDER/plugins/ruby-build
-echo 'export PATH="$RBENV_FOLDER/plugins/ruby-build/bin:$PATH"' >> $RBENV_FILE
+echo 'export PATH="$RBENV_ROOT/plugins/ruby-build/bin:$PATH"' >> $RBENV_FILE
 echo 'eval "$(rbenv init -)"' >> $RBENV_FILE
 
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git $RBENV_FOLDER/plugins/rbenv-gem-rehash
 
+chgrp -R rubies $RBENV_FOLDER
+chmod -R g+rwxXs $RBENV_FOLDER
+
 chmod +x $RBENV_FILE
 
-su vagrant
-source ~/.bashrc
-
-rbenv install 2.1.5
-rbenv global 2.1.5
-ruby -v
-
-if [ $? -gt 0 ]; then
-  echo "Error installing ruby. See log above"
-  exit 1
-fi
-
-# Configure rubygems to ignore documentation and install bundler
-
-echo "gem: --no-ri --no-rdoc" > /home/vagrant/.gemrc
-#echo "Installing bundler"
-#gem install bundler
-#echo "Finish installing bundler"
-
-# Installing Ruby --- End ---
-
-# Install chef solo --- Start ---
-
-echo "Installing chef solo gems"
-/vagrant/install-knife-solo.sh
-echo "Finish installing chef solo gems"
-
-# Install chef solo --- End ---
+echo '---------------------------------'
+echo '    rbenv installed system wide to /usr/local/rbenv'
+echo '    In order to use knife solo, Log in to the machine using "vagrant up" '
+echo '    and run "/vagrant/install-knife-solo.sh '
+echo '---------------------------------'
